566d376db8e4bb4c524a74efa39c5d60b3e0b9b2
Игорь Груздо <igor_gruzdo@bellintegrator.com>
2025-07-01 22:53:23 +0300

    6.7

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"http_server/configs"
+	"http_server/internal/auth"
 	"http_server/internal/hello"
 	"net/http"
 )
@@ -10,6 +11,9 @@ func main() {
 	config := configs.LoadConfig()
 	router := http.NewServeMux()
 	hello.NewHalloHandler(router);
+	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
+		Config: config,
+	})
 
 	server := http.Server{
 		Addr: ":8081",
