a3771c16c48c5a57c5645b8b86558e74ac627fd6
Игорь Груздо <igor_gruzdo@bellintegrator.com>
2025-07-16 15:21:40 +0300

    migrations

---
+++ migrations/auto.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"http_server/internal/link"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&link.Link{})
+}
