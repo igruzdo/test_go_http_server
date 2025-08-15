c540fee593adbfeb6ba8a5b939c7e2b2eb290299
Игорь Груздо <igor_gruzdo@bellintegrator.com>
2025-08-15 17:39:00 +0300

    done 9.5

--- internal/link/handler.go
+++ internal/link/handler.go
@@ -1,6 +1,8 @@
 package link
 
 import (
+	"http_server/pakages/request"
+	"http_server/pakages/response"
 	"net/http"
 )
 
@@ -26,7 +28,19 @@ func NewLinkHandler(router *http.ServeMux, deps LinkHandlerDeps) {
 
 func (handler *LinkHandler) Create() http.HandlerFunc {
 	return func(writer http.ResponseWriter, req *http.Request) {
+		body, err := request.HandleBody[LinkCreateRequest](&writer, req)
+		if err != nil {
+			return
+		}
 
+		link := NewLink(body.Url)
+		createdLink, err := handler.LinkRepository.Create(link)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		response.Json(writer, createdLink, 201)
 	}
 }
 
@@ -44,6 +58,13 @@ func (handler *LinkHandler) Delete() http.HandlerFunc {
 
 func (handler *LinkHandler) GoTo() http.HandlerFunc {
 	return func(writer http.ResponseWriter, req *http.Request) {
+		hash := req.PathValue("hash")
+		link, err := handler.LinkRepository.GetByHash(hash)
 
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusNotFound)
+		}
+
+		http.Redirect(writer, req, link.Url, http.StatusTemporaryRedirect)
 	}
 }
